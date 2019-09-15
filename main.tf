eddbd38d8e714ae17817c800918a5086ed123a03
Khalid Zoabi <kzoabi@outlook.com>
2019-09-14 22:21:04 -0400

    Adding a terraform file that deploys the Lambda function

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_lambda_function" "fast_lambda" {
+  function_name = "fast_lambda"
+  runtime       = "provided"
+  handler       = "bootstrap"
+  filename      = "lambda.zip"
+  source_code_hash = "${filebase64sha256("lambda.zip")}"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
