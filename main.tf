75374887c14e16002f943d114ca96b04b984c97f
Khalid <kzoabi@outlook.com>
2020-12-02 19:39:54 -0500

    Upgrading terraform syntax to latest style

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "aws_lambda_function" "fast_lambda" {
   runtime       = "provided"
   handler       = "bootstrap"
   filename      = "lambda.zip"
-  source_code_hash = "${filebase64sha256("lambda.zip")}"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  source_code_hash = filebase64sha256("lambda.zip")
+  role          = aws_iam_role.iam_for_lambda.arn
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
